718933860ddb0ccc903a00687ce4fd870f92bfef
faithelliott <faith.ann.elliott@ndsu.edu>
2019-11-28 22:05:37 -0600

    cors?

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
-var cors = require('cors');
+var cors = require('cors')({origin: true});
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,6 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
