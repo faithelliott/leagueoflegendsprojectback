6ef502cd8d95f412319aa368f7ecfc8c37f60c42
faithelliott <faith.ann.elliott@ndsu.edu>
2019-11-28 22:07:34 -0600

    heroku

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
+var cors = require('cors')({origin: true});
 
 var data = "";
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.get('/:id', function(req, res, next) {
   request('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ req.params.id +'?api_key=RGAPI-79635536-9e36-487e-80bc-bf945d9f9e6c', { json: true }, (err, res, body) => {
