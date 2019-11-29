46f4fac6a85948bc82a826959baed37880da1a45
faithelliott <faith.ann.elliott@ndsu.edu>
2019-11-28 21:33:27 -0600

    json fix

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,9 @@ router.get('/:id', function(req, res, next) {
   request('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ req.params.id +'?api_key=RGAPI-79635536-9e36-487e-80bc-bf945d9f9e6c', { json: true }, (err, res, body) => {
     data = body; 
     data =JSON.stringify(data);
-    data = data.replace(/<(?:.|\n)*?>/gm, '');
   });
  // res.render('index', {title1:  data.replace('<','')});
- res.json(data);
+ res.send(data);
   console.log('test:'+data.json);
 });
 
