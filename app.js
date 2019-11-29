09693d63c21e9979ab6ff38c54d44202e01ce8ad
faithelliott <faith.ann.elliott@ndsu.edu>
2019-11-28 22:09:40 -0600

    hello

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
-var cors = require('cors')({origin: true});
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
