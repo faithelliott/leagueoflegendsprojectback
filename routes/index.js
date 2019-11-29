e58f26cfc879160cdb9d2987782307cbc85c18c9
faithelliott <faith.ann.elliott@ndsu.edu>
2019-11-28 20:36:15 -0600

    cors

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var cors = require('cors');
+
 
 var data = "";
 
-router.get('/:id',cors(), function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   request('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ req.params.id +'?api_key=RGAPI-79635536-9e36-487e-80bc-bf945d9f9e6c', { json: false }, (err, res, body) => {
     data = body; 
   });
