293ce36c79e594d64c0ab1cc6864448ffeea458c
faithelliott <faith.ann.elliott@ndsu.edu>
2019-12-02 17:58:21 -0600

    added akali mastery

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,30 @@ var request = require('request');
 var cors = require('cors')({origin: true});
 
 var data = "";
-
+var userString ="";
+var akaliData="";
 
 
 router.get('/:id', function(req, res, next) {
   request('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ req.params.id +'?api_key=RGAPI-05a90f72-a07b-4d8f-bcb3-0a8f938d84ab', { json: true }, (err, res, body) => {
     data = body; 
     data =JSON.stringify(data);
+    userString = body.id;
+    console.log(userString);
+
   });
  // res.render('index', {title1:  data.replace('<','')});
  res.send(data);
   console.log('test:'+data.json);
+});
+
+router.get('/:id/:userString',function(req,res,next){
+  request('https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/'+req.params.userString+'/by-champion/84?api_key=RGAPI-05a90f72-a07b-4d8f-bcb3-0a8f938d84ab',{json:true},(err,req,body)=>{
+  akaliData = body;
+  akaliData = JSON.stringify(akaliData);
+  });
+  res.send(akaliData);
+  
 });
 
 function ignoreFavicon(req, res, next) {
