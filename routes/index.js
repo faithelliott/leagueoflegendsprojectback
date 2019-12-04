6a174032cc9ce5b83d0ca2e0b3efa3e0c486f7c2
faithelliott <faith.ann.elliott@ndsu.edu>
2019-12-04 16:34:35 -0600

    match history pull

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,34 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var cors = require('cors')({origin: true});
+require('dotenv').config();
+console.log(process.env);
 
-var data = "";
 var userString ="";
-var akaliData="";
-var api = "RGAPI-70f1d0e8-b272-452b-bb20-2019abde2e7f";
+var accId ="";
+var api ='RGAPI-70f1d0e8-b272-452b-bb20-2019abde2e7f';
 
 //gets basic account datas
-router.get('/:id', function(req, res) {
+router.get('/user/:id', function(req, res) {
   request('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ req.params.id +'?api_key='+api, { json: true }, (err,apiRes,body) => {
     userString = body.id;
+    accId = body.accId;
     console.log(userString);
     res.send(JSON.stringify(body));
   });
 });
 
 //gets akali data
-router.get('/:id/:userString',function(req,res){
+router.get('/user/:id/:userString',function(req,res){
   request('https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/'+req.params.userString+'/by-champion/84?api_key='+api,{json:true},(err,apiRes,body)=>{
+  res.send(JSON.stringify(body));
+  });
+});
+
+//gets akali match data
+router.get('/match/:id/:accId',function(req,res){
+request('https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/'+req.params.accId+'?champion=84&endIndex=10&beginIndex=0&api_key='+api,{json:true},(err,apiRes,body)=>{
+ 
   res.send(JSON.stringify(body));
   });
 });
