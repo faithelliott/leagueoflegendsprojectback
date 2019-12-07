522b6bafa93bcf972195c4ae70a2fc188dc669f5
faithelliott <faith.ann.elliott@ndsu.edu>
2019-12-07 16:24:56 -0600

    added full match data

--- routes/index.js
+++ routes/index.js
@@ -26,11 +26,17 @@ router.get('/user/:id/:userString',function(req,res){
   });
 });
 
+
 //gets akali match data
 router.get('/match/:id/:accId',function(req,res){
 request('https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/'+req.params.accId+'?champion=84&endIndex=10&beginIndex=0&api_key='+api,{json:true},(err,apiRes,body)=>{
- 
   res.send(JSON.stringify(body));
+  });
+});
+
+router.get('/matchdata/:matchID',function(req,res){
+  request('https://na1.api.riotgames.com/lol/match/v4/matches/'+req.params.matchID+'?api_key='+api,{json:true},(err,apiRes,body)=>{
+    res.send(JSON.stringify(body));
   });
 });
 
