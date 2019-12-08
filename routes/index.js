3d110adf639d51ea2ff7abeb76165768106bf987
faithelliott <faith.ann.elliott@ndsu.edu>
2019-12-08 17:37:09 -0600

    a comment

--- routes/index.js
+++ routes/index.js
@@ -34,6 +34,7 @@ request('https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/'+req.
   });
 });
 
+//gets full match data
 router.get('/matchdata/:matchID',function(req,res){
   request('https://na1.api.riotgames.com/lol/match/v4/matches/'+req.params.matchID+'?api_key='+api,{json:true},(err,apiRes,body)=>{
     res.send(JSON.stringify(body));
